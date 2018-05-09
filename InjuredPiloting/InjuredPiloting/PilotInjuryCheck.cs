f1621ebbb21bb99d82779545e0bdbbee71fb363f
RealityMachina <tomclancy1995@gmail.com>
2018-05-08 23:54:10 -0400

    Script commit.

---
+++ InjuredPiloting/InjuredPiloting/PilotInjuryCheck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using BattleTech;
+using System.Reflection;
+
+namespace InjuredPiloting
+{
+    class PilotInjuryCheck
+    {
+        [HarmonyPatch(typeof(BattleTech.Pilot))]
+        [HarmonyPatch("CanPilot", PropertyMethod.Getter)]
+        public static class BattleTech_Pilot_CanPilot_Prefix
+        {
+            static bool Prefix(Pilot __instance, bool __result)
+            {
+                GameInstance battletechGame = UnityGameInstance.BattleTechGame;
+
+                if(battletechGame == null || battletechGame.Simulation == null)
+                {
+                    return true; //ignore the prefix check because the game doesn't exist, or the simulation doesn't
+                }
+
+                //we know we exist now, so...
+
+                int medTechs = battletechGame.Simulation.MedTechs.Count;
+
+                if(medTechs < 3)
+                {
+                    return true; //not enough medtechs to override 
+                }
+
+                int numInjuries = __instance.Injuries;
+
+                if((medTechs / 3) >= numInjuries) //formula is that for every 3 medtechs, we can ignore an injury. So if we have enough medtechs to equal the number of injuries the pilot has...
+                {
+                    __result = true;
+                }
+
+
+                return false;
+            }
+        }
+
+    }
+}
---
+++ InjuredPiloting/InjuredPiloting/initClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Harmony;
+
+namespace InjuredPiloting
+{
+    public class InitClass
+    {
+        public static void Init()
+        {
+            var harmony = HarmonyInstance.Create("Battletech.realitymachina.InjuredPiloting");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
