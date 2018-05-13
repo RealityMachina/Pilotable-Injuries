cc44f98aefc263a6f862aae54a6927ee6972098d
RealityMachina <tomclancy1995@gmail.com>
2018-05-13 15:54:13 -0400

    Attempt at increasing injury recovering times if an injuerd pilot gets hurt.

--- InjuredPiloting/InjuredPiloting/PilotInjuryCheck.cs
+++ InjuredPiloting/InjuredPiloting/PilotInjuryCheck.cs
@@ -51,5 +51,95 @@ namespace InjuredPiloting
             }
         }
 
+        [HarmonyPatch(typeof(BattleTech.Pilot))]
+        [HarmonyPatch("InjurePilot")]
+        public static class BattleTech_Pilot_InjurePilot_Prefix
+        {
+            static void Prefix(Pilot __instance, string sourceID, int stackItemUID, int dmg, string source)
+            {
+                if (__instance.ParentActor != null && __instance.ParentActor.StatCollection.GetValue<bool>("IgnorePilotInjuries"))
+                {
+                    return;
+                }
+                int bonusHealth = __instance.BonusHealth;
+                if (bonusHealth > 0)
+                {
+                    int num = Mathf.Min(dmg, bonusHealth);
+                    dmg -= num;
+
+                }
+
+                if(dmg <= 0)
+                {
+                    return;
+                }
+                //we're getting injured, so...
+                Holder.newlyInjured.Add(__instance);
+            }
+        }
+
+
+        [HarmonyPatch(typeof(BattleTech.GameInstance), "LaunchContract", new Type[] { typeof(Contract), typeof(string) })]
+        public static class BattleTech_GameInstance_LaunchContract_Patch
+        {
+            static void Postfix(GameInstance __instance, Contract contract)
+            {
+                // reset on new contracts
+                Holder.Reset();
+
+                foreach (Pilot pilot in __instance.Simulation.PilotRoster)
+                {
+                    if(pilot.Injuries > 0)
+                    {
+                        Holder.injPilots.Add(pilot);
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(BattleTech.SimGameState))]
+        [HarmonyPatch("RefreshInjuries")]
+        public static class BattleTech_RefreshInjuries_Patch
+        {
+            static void Postfix(SimGameState __instance)
+            {
+                foreach (Pilot pilot in Holder.injPilots)
+                {
+                    if(Holder.newlyInjured.Contains(pilot))
+                    {
+                        //an already injured pilot has been injured, so...
+                        WorkOrderEntry_MedBayHeal workOrderEntry_MedBayHeal;
+                        if (!__instance.MedBayQueue.SubEntryContainsID(pilot.Description.Id))
+                        {
+                            return;
+                        }
+                        //we now know they have an injury queue, so...
+                        workOrderEntry_MedBayHeal = (WorkOrderEntry_MedBayHeal)__instance.MedBayQueue.GetSubEntry(pilot.Description.Id);
+
+                        workOrderEntry_MedBayHeal.SetCost(GetInjuryCost(pilot, __instance));
+                    }
+                }
+            }
+
+            private static int GetInjuryCost(Pilot p, SimGameState __instance)
+            {
+                int num = 0;
+                if (p.LethalInjuries)
+                {
+                    num += __instance.Constants.Pilot.LethalDamageCost;
+                }
+                if (p.IsIncapacitated)
+                {
+                    num += __instance.Constants.Pilot.IncapacitatedDamageCost;
+                }
+                int num2 = Mathf.Min(p.Injuries, p.Health);
+                int num3 = __instance.Constants.Pilot.BaseInjuryDamageCost / p.Health;
+                for (int i = 0; i < num2; i++)
+                {
+                    num += num3;
+                }
+                return num + p.pilotDef.TimeoutRemaining * __instance.GetDailyHealValue();
+            }
+        }
     }
 }
--- InjuredPiloting/InjuredPiloting/initClass.cs
+++ InjuredPiloting/InjuredPiloting/initClass.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Reflection;
 using Harmony;
+using BattleTech;
 
 namespace InjuredPiloting
 {
@@ -15,4 +16,15 @@ namespace InjuredPiloting
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
+
+    public class Holder
+    {
+        public static HashSet<Pilot> injPilots; //use this to track already injured pilots on a mission
+        public static HashSet<Pilot> newlyInjured; //use this to track pilots who already gotten hit
+        public static void Reset()
+        {
+            injPilots = new HashSet<Pilot>();
+        }
+    }
+
 }
